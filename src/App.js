a86da8df26ceb359f114f084429378c3baeaeafd
Luciano Erick Figueiredo <luciano.erick.filho@ccc.ufcg.edu.br>
2022-11-07 09:53:53 -0300

    Adding /home route

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import UserAccess from './pages/UserAccess';
-import { PrivateRoute } from './util/Auth';
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import UserAccess from "./pages/UserAccess";
+import { PrivateRoute } from "./util/Auth";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/user_access' element={<UserAccess />} />
+        <Route path="/user_access" element={<UserAccess />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );

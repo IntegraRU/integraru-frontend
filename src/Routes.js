f7aeae742c72064389b186735862f82d195a729c
Luciano Erick Figueiredo <luciano.erick.filho@ccc.ufcg.edu.br>
2022-11-27 21:06:18 -0300

    Adding route to checkout page

--- src/Routes.js
+++ src/Routes.js
@@ -4,8 +4,9 @@ import { GiKnifeFork } from "react-icons/gi";
 import Home from "./pages/Home";
 import ViewMenus from "./pages/ViewMenus";
 import ChangeMenu from "./pages/ChangeMenu";
-import Profile from './pages/Profile';
+import Profile from "./pages/Profile";
 import UserMenus from "./pages/UserMenus";
+import Checkout from "./pages/Checkout";
 
 export const serviceRoutes = [
   {
@@ -13,48 +14,56 @@ export const serviceRoutes = [
     route: "inicio",
     icon: BiHome,
     component: Home,
-    type: 'user',
+    type: "user",
   },
   {
     name: "Perfil",
     route: "perfil",
     icon: BiUser,
     component: Profile,
-    type: 'user',
+    type: "user",
   },
   {
     name: "Página inicial",
     route: "inicio",
     icon: BiHome,
     component: Home,
-    type: 'admin',
+    type: "admin",
   },
   {
     name: "Cadastrar Cardápio",
     route: "cardapio/novo",
     icon: GiKnifeFork,
     component: ChangeMenu,
-    type: 'admin',
+    type: "admin",
   },
   {
     name: "Editar Cardápio",
     route: "cardapio/editar",
     icon: GiKnifeFork,
     component: ChangeMenu,
-    type: 'admin',
+    type: "admin",
   },
   {
     name: "Visualizar Cardápios",
     route: "cardapio",
     icon: GiKnifeFork,
     component: ViewMenus,
-    type: 'admin',
+    type: "admin",
   },
   {
     name: "Visualizar Cardápios",
     route: "cardapio",
     icon: GiKnifeFork,
     component: UserMenus,
-    type: 'user',
-  }
+    type: "user",
+  },
+  {
+    name: "Checkout",
+    route: "checkout",
+    icon: GiKnifeFork,
+    component: Checkout,
+    type: "user",
+    directAccess: false,
+  },
 ];

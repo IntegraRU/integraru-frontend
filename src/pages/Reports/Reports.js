2f53b63cbf0d08165b8b723b42793269577dd9b8
Franklin Regis <franklin.oliveira@ccc.ufcg.edu.br>
2023-03-21 21:15:29 -0300

    fix: remove report placeholders

--- src/pages/Reports/Reports.js
+++ src/pages/Reports/Reports.js
@@ -6,7 +6,7 @@ import { BsFillArrowLeftCircleFill, BsFillArrowRightCircleFill } from 'react-ico
 import Meal from '../../assets/food.png';
 import { Bar, BarChart, ResponsiveContainer, XAxis, YAxis, Cell } from 'recharts'
 import { Carousel } from 'react-responsive-carousel';
-import { format, isAfter, isBefore, parse, parseISO } from 'date-fns'
+import { isAfter, isBefore, parse, parseISO } from 'date-fns'
 import api from '../../services/api';
 import { useCallback } from "react";
 
@@ -15,11 +15,9 @@ const horariosAlmoco = ['11:30', '12:00', '12:30', '13:00', '13:30', '14:00'];
 const horariosJantar = ['17:00', '17:30', '18:00', '18:30', '19:00'];
 
 export default function Reports() {
-    // const [allMenus, setAllMenus] = useState([{"id":1,"tipo":"COMUM","modalidadePrato":"ALMOCO","nome":"dgdg","itens":"dfgdf","urlImagem":"https://assets.unileversolutions.com/recipes-v2/54349.jpg","data":"23/12/2022"}]);
     const [allMenus, setAllMenus] = useState([]);
     const [currentMenu, setCurrentMenu] = useState(0);
     const [currentComment, setCurrentComment] = useState(0);
-    // const [menuInfo, setMenuInfo] = useState({"avaliacaoMedia":3.0,"avaliacoesQuant":[3],"avaliacoesComentarios":["meh"],"checkouts":["2022-12-23T12:15:22.793"],"comensais":1,"taxaAvaliacao":100.0,"taxaComentario":100.0,"taxaOptantes":100.0});
     const [menuInfo, setMenuInfo] = useState();
     useEffect(() => {
         const fetchMenus = async () => {
@@ -68,7 +66,11 @@ export default function Reports() {
         // Dada a lista de horários e checkouts, busca a quantidade de checkouts no intervalo de 30min
         // por exemplo, de 11:30 a 12:00 (i, i+1), pega todos os checkouts, independente do dia, entre esse horário.
         const horariosParsed = horarios.map(horario => parse(horario, 'HH:mm', new Date()));
-        const checkouts = menuInfo.checkouts.map(horario => parseISO(horario));
+        const checkouts = menuInfo.checkouts.map(horario => {
+            const checkout = parseISO(horario);
+            checkout.setHours(checkout.getHours() - 3);
+            return checkout;
+        });
 
         for(let i=0; i < horarios.length - 1; i++){
             const qtd = checkouts.filter(checkout => isAfter(checkout, horariosParsed[i]) && isBefore(checkout, horariosParsed[i+1])).length;
@@ -77,38 +79,6 @@ export default function Reports() {
                 qtd
             }
         }
-
-        //PLACEHOLDER
-        // const result = [
-        //     {
-        //         time: "11:00",
-        //         qtd: 30
-        //     },
-        //     {
-        //         time: "11:30",
-        //         qtd: 50
-        //     },
-        //     {
-        //         time: "12:00",
-        //         qtd: 120
-        //     },
-        //     {
-        //         time: "12:30",
-        //         qtd: 150
-        //     },
-        //     {
-        //         time: "13:00",
-        //         qtd: 100
-        //     },
-        //     {
-        //         time: "13:30",
-        //         qtd: 80
-        //     },
-        //     {
-        //         time: "14:00",
-        //         qtd: 20
-        //     }
-        // ];
         return result;
     }, [allMenus, currentMenu, menuInfo?.checkouts]);
 
